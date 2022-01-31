b3e410d9596c5a49a131dd8f5c475c2c96453978
Samuel Espinoza <samuelesponline@gmail.com>
2022-01-31 15:53:37 -0500

    ready

--- primenumbers.js
+++ primenumbers.js
@@ -1,32 +1,16 @@
-const isPrime=(number)=> {    
-    for(let i = 2,root=Math.sqrt(number); i <= root; i++)
-        if(number % i === 0) return false;
+const isPrime = (number) => {
+    for (let i = 2, root = Math.sqrt(number); i <= root; i++)
+        if (number % i === 0) return false;
     return number > 1;
-}  
+};
 
-const searchPrime=(n)=>{
-  let arr_prime = [];
-  for (let x=0;;x++) {
-      if (isPrime(x)) arr_prime.push(x);
-    if(arr_prime.length==n) break;
-  }
-  return arr_prime;
-}
+const searchPrime = (n) => {
+    let arr_prime = [];
+    for (let x = 0;; x++) {
+        if (isPrime(x)) arr_prime.push(x);
+        if (arr_prime.length == n) break;
+    }
+    return arr_prime;
+};
 
 console.log(searchPrime(20));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
